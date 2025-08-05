1abda75abe2272580f8d4b1fae40124160bfcd26
joaopaulo1706 <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-08-05 14:16:23 -0300

    versão com o secualyze

--- models/tanqueModel.js
+++ models/tanqueModel.js
@@ -1,51 +1,35 @@
-const db = require('../config/db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
 
-const Tanque = {
-    create: (tanque, callback) => {
-        const query = 'INSERT INTO tanques (nome, descricao, preco, quantidade, categoria) VALUES (?, ?, ?, ?, ?)';
-        db.query(query, [tanque.nome, tanque.descricao, tanque.preco, tanque.quantidade, tanque.categoria], (err, results) => {
-            if (err) return callback(err);
-            callback(null, results.insertId);
-        });
-    },
-
-    findById: (id, callback) => {
-        const query = 'SELECT tanques.*, categorias.nome AS categoria_nome FROM tanques JOIN categorias ON tanques.categoria = categorias.id WHERE tanques.id = ?';
-        db.query(query, [id], (err, results) => {
-            if (err) return callback(err);
-            callback(null, results[0]);
-        });
-    },
-
-    getAll: (categoria, callback) => {
-        let query = `
-            SELECT tanques.id, tanques.nome, tanques.descricao, tanques.preco, tanques.quantidade, categorias.nome AS categoria_nome
-            FROM tanques
-            JOIN categorias ON tanques.categoria = categorias.id`;
-
-        if (categoria) query += ' WHERE tanques.categoria = ?';
-
-        db.query(query, [categoria], (err, results) => {
-            if (err) return callback(err);
-            callback(null, results);
-        });
-    },
-
-    update: (id, tanque, callback) => {
-        const query = 'UPDATE tanques SET nome = ?, descricao = ?, preco = ?, quantidade = ?, categoria = ? WHERE id = ?';
-        db.query(query, [tanque.nome, tanque.descricao, tanque.preco, tanque.quantidade, tanque.categoria, id], (err, results) => {
-            if (err) return callback(err);
-            callback(null, results);
-        });
-    },
-
-    delete: (id, callback) => {
-        const query = 'DELETE FROM tanques WHERE id = ?';
-        db.query(query, [id], (err, results) => {
-            if (err) return callback(err);
-            callback(null, results);
-        });
-    }
-};
+const Tanque = sequelize.define('Tanque', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  preco: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  quantidade: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  categoria: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  tableName: 'tanques',
+  timestamps: false
+});
 
 module.exports = Tanque;
--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,75 +1,27 @@
-const db = require('../config/db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
 
-const Usuario = {
-    create: (usuario, callback) => {
-        const query = 'INSERT INTO usuarios (usuarioname, password, role) VALUES (?, ?, ?)';
-        db.query(query, [usuario.usuarioname, usuario.password, usuario.role], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results.insertId);
-        });
-    },
-
-    findById: (id, callback) => {
-        const query = 'SELECT * FROM usuarios WHERE id = ?';
-        db.query(query, [id], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results[0]);
-        });
-    },
-
-    findByUsuarioname: (usuarioname, callback) => {
-        const query = 'SELECT * FROM usuarios WHERE usuarioname = ?';
-        db.query(query, [usuarioname], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results[0]);
-        });
-    },
-
-    update: (id, usuario, callback) => {
-        const query = 'UPDATE usuarios SET usuarioname = ?, password = ?, role = ? WHERE id = ?';
-        db.query(query, [usuario.usuarioname, usuario.password, usuario.role, id], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results);
-        });
-    },
-
-    delete: (id, callback) => {
-        const query = 'DELETE FROM usuarios WHERE id = ?';
-        db.query(query, [id], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results);
-        });
-    },
-
-    getAll: (callback) => {
-        const query = 'SELECT * FROM usuarios';
-        db.query(query, (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results);
-        });
-    },
-
-    searchByName: (name, callback) => {
-        const query = 'SELECT * FROM usuarios WHERE usuarioname LIKE ?';
-        db.query(query, [`%${name}%`], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, results);
-        });
-    },    
-};
+const Usuario = sequelize.define('Usuario', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'usuarios',
+  timestamps: false
+});
 
 module.exports = Usuario;
